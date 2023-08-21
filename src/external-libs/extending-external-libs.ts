79a3342b930ab3a4d6d4f3bb7b55a80240f12668
vitorvitor07 <vitorsilva_53@hotmail.com>
2023-08-21 11:48:32 -0300

    Aula 341 completa

---
+++ src/external-libs/extending-external-libs.ts
@@ -0,0 +1,8 @@
+import _ from "lodash";
+
+const arr = [10, 20, 30];
+
+console.log(_.sum(arr)); // Soma
+console.log(_.min(arr)); // Mínimo
+console.log(_.max(arr)); // Máximo
+console.log(_.mean(arr)); // Média
